bc110b456c1475c71e7a896ac4a53e9ac7d41250
Dmitrii <dmitriy.petrov.03@list.ru>
2022-10-10 22:25:57 +0300

    update lab2

--- laba2/src/Lab1.java
+++ laba2/src/Lab1.java
@@ -1,6 +1,21 @@
 import java.util.Scanner;
 
 public class Lab1 {
+
+    static public double computeArea(Point3d point1, Point3d point2, Point3d point3){
+
+        double a = point1.distanceTo(point2);
+        double b = point1.distanceTo(point3);
+        double c = point2.distanceTo(point3);
+        double p = (a+b+c)/2;
+
+        if (point1.compare(point2) | point1.compare(point3) | point2.compare(point3)){
+            System.out.println("Пользователь, вы ввели такие точки, что какая-то из точек равна другой!");
+            return 1;
+        }
+        return (Math.sqrt(p*(p-a)*(p-b)*(p-c)));
+
+    }
     static public void main(String[] args){
         Scanner in = new Scanner(System.in);
 
@@ -10,20 +25,8 @@ public class Lab1 {
 
         in.close();
 
-    }
-
-    static public void computeArea(Point3d point1, Point3d point2, Point3d point3){
-
-        double a = point1.distanceTo(point2);
-        double b = point1.distanceTo(point3);
-        double c = point2.distanceTo(point3);
-        double p = (a+b+c)/2;
-
-        if (point1.compare(point2) | point1.compare(point3) | point2.compare(point3)){
-            System.out.println("Пользователь, вы ввели такие точки, что какая-то из точек равна другой!");
-            return;
-        }
-        System.out.println(Math.sqrt(p*(p-a)*(p-b)*(p-c)));
+        System.out.println(computeArea(point1, point2, point3));
 
     }
+
 }
