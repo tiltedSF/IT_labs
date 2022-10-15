84d0b182ea58a79b9417ce1a4319b223bc9d1371
Dmitrii <dmitriy.petrov.03@list.ru>
2022-10-15 10:08:27 +0300

    add extends

--- laba2/src/Point3d.java
+++ laba2/src/Point3d.java
@@ -1,42 +1,21 @@
- public class Point3d {
-    private double xCoord;
-    private double yCoord;
+ public class Point3d extends Point2d{
     private double zCoord;
-
     public Point3d(double x, double y, double z) {
-        xCoord = x;
-        yCoord = y;
+        super(x,y);
         zCoord = z;
     }
 
     public Point3d () {
         this(0, 0, 0);
     }
-     // get-еры
-    public double getX () {
-        return xCoord;
-    }
-
-    public double getY () {
-        return yCoord;
-    }
 
      public double getZ () {
          return zCoord;
      }
-    // set-еры
-    public void setX ( double val) {
-        xCoord = val;
-    }
-
-    public void setY ( double val) {
-        yCoord = val;
-    }
 
     public void setZ ( double val) {
          zCoord = val;
     }
-    //compare 2 obh
     public boolean compare( Point3d obj){
         return (this.getX() == obj.getX()) &
                 (this.getY() == obj.getY()) &
